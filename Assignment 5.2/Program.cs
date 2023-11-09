05cb9548238c7aab553515571050bbde30da34b5
NIRMALRAJ1506 <nirmalraj75500@gmail.com>
2023-11-09 16:39:24 +0530

    First Commit of Assignment5.2

---
+++ Assignment 5.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_5._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int s = 0,t=1;
+            Console.WriteLine("Fibonacci Series");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(s);
+                int temp = s;
+                s = t;
+                t = temp + t;
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
